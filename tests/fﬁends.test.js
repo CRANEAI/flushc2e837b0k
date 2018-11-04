8a91b38a57da1f5680ea3e6aa58cc7a8206d0734
Elly <elly@mg.crane.ai>
2018-11-03 20:11:44 -0400

    added tests/fﬁends refactoring

---
+++ tests/fﬁends.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - fﬁends UNIT TESTS
+ * Application Client
+ * flush
+*/
+
+/* fﬁends view has Active Label located at the bottom left of the view, text for button needs identified */
+it("fﬁends view has Active Label located at the bottom left of the view, text for button needs identified", () => {
+  /* ToDo developer needs to complete unit test 
+               fﬁends view has Active Label located at the bottom left of the view, text for button needs identified
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
